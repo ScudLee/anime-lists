764df6797ad86c33b5142d86883431dec9fa324c
Stephen L Jones <sljones@slohj.org>
2019-07-05 22:25:32 -0500

    Add useragent string to curl
    Anidb recently changed policy on the animetitles.xml access and requires
    that the requesting project is announced in the useragent string.

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-curl -o animetitles.xml --compressed http://anidb.net/api/animetitles.xml.gz
+curl -A Scudlee/anime-lists -o animetitles.xml --compressed http://anidb.net/api/anime-titles.xml.gz
 
 xsltproc -o animetitles.xml transforms/sort-animetitles.xsl animetitles.xml
 xsltproc -o anime-list-master.xml transforms/update-anime-list-master.xsl animetitles.xml

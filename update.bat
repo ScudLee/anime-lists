945ce805f2412155c8fa25e23ad653087fbfeb8a
C. Kloppinger (Nanashi Mumei) purposelycryptic@users.noreply.github.com <purposelycryptic@users.noreply.github.com>
2019-07-10 17:39:14 -0400

    Changes from issues, etc.

--- update.bat
+++ update.bat
@@ -1,6 +1,6 @@
 @echo off
 
-curl -o animetitles.xml --compressed http://anidb.net/api/animetitles.xml.gz
+curl -A Scudlee/anime-lists -o animetitles.xml --compressed http://anidb.net/api/anime-titles.xml.gz
 
 xsltproc -o animetitles.xml transforms/sort-animetitles.xsl animetitles.xml
 xsltproc -o anime-list-master.xml transforms/update-anime-list-master.xsl animetitles.xml

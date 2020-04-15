d930acc7d1c508a5a41422e93a9aa71c6a0058cb
Stephen L Jones <sljones@slohj.org>
2020-04-15 10:46:00 -0500

    Fix update.sh for https
    curl does not automatically decompress compressed files over http2
    streams.  A decision needs to be made about wich windows tool to use for
    decompression.

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-curl -A Scudlee/anime-lists -o animetitles.xml --compressed http://anidb.net/api/anime-titles.xml.gz
+curl -A Scudlee/anime-lists --compressed https://anidb.net/api/anime-titles.xml.gz | gzip -dc > animetitles.xml
 
 xsltproc -o animetitles.xml transforms/sort-animetitles.xsl animetitles.xml
 xsltproc -o anime-list-master.xml transforms/update-anime-list-master.xsl animetitles.xml

e619d47fc69edd5712fae815932cd7c3517504ff
reconman <reconman@users.noreply.github.com>
2020-12-19 13:28:52 +0100

    Remove commit steps from update scripts

--- update.bat
+++ update.bat
@@ -5,13 +5,3 @@ curl -A Scudlee/anime-lists -o animetitles.xml --compressed http://anidb.net/api
 xsltproc -o animetitles.xml transforms/sort-animetitles.xsl animetitles.xml
 xsltproc -o anime-list-master.xml transforms/update-anime-list-master.xsl animetitles.xml
 call generate-lists.bat
-
-call git add "animetitles.xml"
-call git add "anime-list-master.xml"
-call git add "anime-list.xml"
-call git add "anime-list-full.xml"
-call git add "anime-movieset-list.xml"
-call git add "anime-list-todo.xml"
-call git add "anime-list-unknown.xml"
-
-call git commit -m "Update to latest animetitles.xml"
--- update.sh
+++ update.sh
@@ -5,13 +5,3 @@ curl -A Scudlee/anime-lists --compressed https://anidb.net/api/anime-titles.xml.
 xsltproc -o animetitles.xml transforms/sort-animetitles.xsl animetitles.xml
 xsltproc -o anime-list-master.xml transforms/update-anime-list-master.xsl animetitles.xml
 ./generate-lists.sh
-
-git add "animetitles.xml"
-git add "anime-list-master.xml"
-git add "anime-list.xml"
-git add "anime-list-full.xml"
-git add "anime-movieset-list.xml"
-git add "anime-list-todo.xml"
-git add "anime-list-unknown.xml"
-
-git commit -m "Update to latest animetitles.xml"
